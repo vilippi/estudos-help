05f0d298d11de67f679facaf28dcc55bff36af01
Vitor Lippi <vitorlippilima.vl@gmail.com>
2026-02-12 13:36:56 -0300

    Inicio do Projeto

---
+++ frontend/app/layout.tsx
@@ -0,0 +1,15 @@
+import "./globals.css";
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="pt-br" suppressHydrationWarning>
+      <body>
+        {children}
+      </body>
+    </html>
+  );
+}
